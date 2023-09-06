86baa0b09165506b4c62d00c88cdc9d247a0b52f
Arnob <arnobtuhin.j@gamil.com>
2023-09-06 21:36:01 +0600

    get method added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+
+app.use(morgan("dev"));
+
+
+app.get("/test", (req, res)=>{
+    res.status(200).send({
+        massage: "Welcome to the Myshop Server site",
+    });
+});
+
+
 
 
 
